db23175e3415bd424ef22a80821e145ad4df8cfb
durga2706 <durkadevi0610@gmail.com>
2026-06-10 19:44:36 +0530

    Create c_problem13.c

---
+++ c_problem13.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num,reverse,digit;
+    reverse=0;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    while(num>0)
+    {
+        digit=num%10;
+        reverse=reverse*10+digit;
+        num=num/10;
+    }
+    printf("%d",reverse);
+    return 0;
+}
