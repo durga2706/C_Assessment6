419fe68dcb7ee6c530ad788e865418c734bbc16f
durga2706 <durkadevi0610@gmail.com>
2026-06-13 14:23:33 +0530

    Create c_problem25.c

---
+++ c_problem25.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,digit;
+    int count=0;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        digit=n%10;
+        if(digit==2||digit==3||digit==5||digit==7)
+        {
+            count++;
+        }
+        n=n/10;
+    }
+    printf("%d",count);
+    return 0;
+}
